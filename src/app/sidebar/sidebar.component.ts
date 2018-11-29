e0235342496a780c955f0614cc178eeb2ac53617
skaggsw <wskaggs3262@gmail.com>
2018-11-29 08:04:01 -0600

    Update sidebar list with current place in feed, scroll to position when month clicked

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import {consoleTestResultHandler} from 'tslint/lib/test';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,113 +8,38 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  FIELD_TOP = 80;
+  FIELD_TOP = 70;
 
-  constructor() {
-    this.monthHighlightingClassMap.set('august', 'unselected-li');
-    this.monthHighlightingClassMap.set('september', 'unselected-li');
-    this.monthHighlightingClassMap.set('october', 'unselected-li');
-    this.monthHighlightingClassMap.set('november', 'unselected-li');
-    this.monthHighlightingClassMap.set('december', 'unselected-li');
-    this.monthHighlightingClassMap.set('january', 'unselected-li');
-    this.monthHighlightingClassMap.set('february', 'unselected-li');
-    this.monthHighlightingClassMap.set('march', 'unselected-li');
-    this.monthHighlightingClassMap.set('april', 'unselected-li');
-    this.monthHighlightingClassMap.set('may', 'unselected-li');
-    this.monthHighlightingClassMap.set('june', 'unselected-li');
-    this.monthHighlightingClassMap.set('july', 'unselected-li');
-  }
+  monthNames = ['August', 'September', 'October', 'November', 'December',
+                'January', 'February', 'March', 'April', 'May', 'June', 'July' ];
 
-  @ViewChild('feed')  feedContent;
+  constructor() {}
+
+  monthHighlightingClass: boolean[] = [false, false, false, false, false, false, false, false, false, false, false, false];
+
+  @ViewChild('feed')            feedContent;
+  @ViewChild('feedContainer')  feedScrollingContainer;
 
   pendingClass = 'selected-li';
   sentClass = 'unselected-li';
 
-  monthHighlightingClassMap = new Map<string, string>();
-
   sentMonthVisibility = 'hidden';
 
   checkScroll() {
-    const sentMarkerPosition      = this.feedContent.sentMarker.nativeElement.getBoundingClientRect().top;
-
-    // console.log(this.feedContent.septemberMarker.nativeElement.getBoundingClientRect().top);
+    const sentMarkerPosition      = this.feedContent.sentMarker.first.nativeElement.getBoundingClientRect().top;
 
     if (sentMarkerPosition <= this.FIELD_TOP) {
       this.pendingClass  = 'unselected-li';
       this.sentClass     = 'selected-li';
       this.sentMonthVisibility = 'visible';
-      if (this.feedContent.augustMarker != null &&
-        this.feedContent.augustMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
+
+      this.monthHighlightingClass = [false, false, false, false, false, false, false, false, false, false, false, false];
+      for (let i = 11; i >= 0; i--) {
+        if (this.feedContent.markers[i].first != null &&
+            this.feedContent.markers[i].first.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
+          this.monthHighlightingClass[i] = true;
+          break;
         }
-        this.monthHighlightingClassMap.set('august', 'selected-li');
-      } else if (this.feedContent.septemberMarker != null &&
-        this.feedContent.septemberMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('september', 'selected-li');
-      } else if (this.feedContent.octoberMarker != null &&
-        this.feedContent.octoberMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('october', 'selected-li');
-      } else if (this.feedContent.novemberMarker != null &&
-        this.feedContent.novemberMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('november', 'selected-li');
-      } else if (this.feedContent.decemberMarker != null &&
-        this.feedContent.decemberMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('december', 'selected-li');
-      } else if (this.feedContent.januaryMarker != null &&
-        this.feedContent.januaryMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('january', 'selected-li');
-      } else if (this.feedContent.februaryMarker != null &&
-        this.feedContent.februaryMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('february', 'selected-li');
-      } else if (this.feedContent.marchMarker != null &&
-        this.feedContent.marchMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('march', 'selected-li');
-      } else if (this.feedContent.aprilMarker != null &&
-        this.feedContent.aprilMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('april', 'selected-li');
-      } else if (this.feedContent.mayMarker != null &&
-        this.feedContent.mayMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('may', 'selected-li');
-      } else if (this.feedContent.juneMarker != null &&
-        this.feedContent.juneMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('june', 'selected-li');
-      } else if (this.feedContent.julyMarker != null &&
-        this.feedContent.julyMarker.nativeElement.getBoundingClientRect().top <= this.FIELD_TOP) {
-        for (const key of Array.from(this.monthHighlightingClassMap.keys())) {
-          this.monthHighlightingClassMap.set(key, 'unselected-li');
-        }
-        this.monthHighlightingClassMap.set('july', 'selected-li');
       }
     } else {
       this.pendingClass  = 'selected-li';
@@ -125,4 +51,12 @@ export class SidebarComponent implements OnInit {
   ngOnInit() {
   }
 
+  onMonthClicked(monthName) {
+    for (let i = 0; i < 12; i++) {
+      if (monthName === this.monthNames[i] && this.feedContent.markers[i].first != null) {
+        this.feedContent.markers[i].first.nativeElement.scrollIntoView({ behavior: 'smooth'});
+        break;
+      }
+    }
+  }
 }

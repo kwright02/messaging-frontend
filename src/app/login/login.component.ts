a8b696ffe1c01a9c610f60cfecad6fdbb0982462
skaggsw <wskaggs3262@gmail.com>
2019-01-29 08:37:40 -0600

    Change login fields to integrate google sign-in

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('login') googleLoginButton;
 
-  ngOnInit() {
+  private clientId = '180374672702-4543o9ejllfs092e7snd8fjvnf12pf6g.apps.googleusercontent.com';
+
+  private scope = [
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/contacts.readonly',
+    'https://www.googleapis.com/auth/admin.directory.user.readonly'
+  ].join(' ');
+
+  public auth2: any;
+
+  public googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: this.clientId,
+        cookie_policy: 'single_host_origin',
+        scope: this.scope
+      });
+      this.attachSignin(this.element.nativeElement.firstChild.children[1].firstChild);
+    });
   }
 
+  public attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+        const profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+
+        console.log('ID: ' + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+
+        // ...
+      }, function (error) {
+        console.log(JSON.stringify(error, undefined, 2));
+      });
+  }
+
+  constructor(private element: ElementRef) {
+    console.log('ElementRef: ', this.element);
+    // console.log('LoginElementRef: ', this.googleLoginButton);
+  }
+
+  ngAfterViewInit() {
+    this.googleInit();
+    console.log('ElementRef: ', this.element.nativeElement.firstChild.children[1].firstChild);
+  }
 }
